73db2720719c146cf74702bb51a6b45e5e2e4123
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-04-10 23:12:50 +0300

    update Form component

--- 02-contacts-app/src/components/Contacts/Form/index.js
+++ 02-contacts-app/src/components/Contacts/Form/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import {useState} from 'react';
 
 function Form() {
+    const [form, setForm] = useState({fullName: '', phone_number:'' });
+
+    const onChangeInput = (arr) =>{
+        setForm({...form,[arr.target.name]:arr.target.value});
+    };
+
+    const onSubmit = (e) => {
+    //console.log(form);
+       e.preventDefault();
+       if(form.fullName === '' || form.phone_number === ''){
+        // console.log('do not work');
+        return false;
+       }
+    }
+
   return (
-    <div>Form</div>
+    <form onSubmit={onSubmit}>
+        <div>
+        <input 
+        name='fullName' 
+        placeholder='Full Name'
+        onChange={onChangeInput}
+         />
+        </div>
+        <div>
+        <input 
+        name='phone_number' 
+        placeholder='Phone Number'
+        onChange={onChangeInput}
+         />
+        </div>
+
+
+        <button>ADD CONTACT</button>
+    </form>
   );
 }
 
