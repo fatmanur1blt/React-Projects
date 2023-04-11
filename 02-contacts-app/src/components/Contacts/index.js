0c219a64a2a6d77c3b67a88d3166ab84df7178f2
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-04-12 01:11:59 +0300

    update file

--- 02-contacts-app/src/components/Contacts/index.js
+++ 02-contacts-app/src/components/Contacts/index.js
@@ -4,14 +4,27 @@ import List from './List';
 import Form from './Form';
 
 function Contacts() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            fullName:"Fatmanur",
+            phone_number:12345,
+        },
+        {
+            fullName:"Jane",
+            phone_number:9876,
+        },
+        {
+            fullName:"Mehmet",
+            phone_number:63465873,
+        },
+    ]);
 
     useEffect(() =>{
         console.log(contacts);
     },[contacts]);
   return (
     <div>
-        <List />
+        <List contacts={contacts}/>
         <Form addContacts = {setContacts} contacts={contacts}/>
     </div>
   );
