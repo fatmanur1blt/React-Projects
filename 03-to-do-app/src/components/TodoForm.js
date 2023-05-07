7b201551a861b995418c7a308e354efd371324b5
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-05-07 14:22:25 +0300

    update form component

---
+++ 03-to-do-app/src/components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react'
+
+function TodoForm({addTodo}) {
+
+    const [value, setValue] = useState("");
+
+    const submitForm = e =>{
+        e.preventDefault();
+        addTodo(value);
+        setValue("");
+    }
+  return (
+    <form onSubmit={submitForm}>
+        <h1>TO-DO-APP</h1>
+
+        <input type='text' 
+        placeholder='add something'
+        className='todo-input'
+        value={value}
+        onChange={(e)=>setValue(e.target.value)}
+        />
+        <button type='submit' className='todo-button'>ADD</button>
+    </form>
+  )
+}
+
+export default TodoForm;
