5b35580cb0ae8274c5ff55b8fcf1258e49667571
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-05-13 12:11:23 +0300

    update edit component

--- 03-to-do-app/src/components/TodoEdit.js
+++ 03-to-do-app/src/components/TodoEdit.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function TodoEdit() {
+function TodoEdit({editTodo,task}) {
+
+    const [value, setValue] = useState(task.task);
+
+    const submitForm = e =>{
+        e.preventDefault();
+        editTodo(value, task.id);
+        setValue("");
+    }
   return (
-    <div>TodoEdit</div>
+    <form onSubmit={submitForm}>
+
+        <input type='text' 
+        placeholder='update'
+        className='todo-input'
+        value={value}
+        onChange={(e)=>setValue(e.target.value)}
+        />
+        <button type='submit' className='todo-button'>UPDATE</button>
+    </form>
   )
 }
 
