1bd569da254388b45f8ab5f65cca45be9577dcc5
Fatmanur Bulut <bfatmanur38@gmail.com>
2023-05-14 17:49:34 +0300

    update files

--- 03-to-do-app/src/components/Todo.js
+++ 03-to-do-app/src/components/Todo.js
@@ -6,8 +6,11 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons';
 function Todo({task,todoComplete, deleteTodo, editTodo}) {
   return (
     <div className='todo'>
+   
+      <div className='todo-p'>
       <p onClick={() => todoComplete(task.id)}
          className={`${task.completed ? 'completed':""}`}>{task.task}</p>
+      </div>
       <div>
         <FontAwesomeIcon className='font' icon={faPenToSquare} 
           onClick={()=> editTodo(task.id)}/>
--- 03-to-do-app/src/components/TodoInclusive.js
+++ 03-to-do-app/src/components/TodoInclusive.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import TodoForm from './TodoForm';
 import Todo from './Todo';
 import TodoEdit from './TodoEdit';
@@ -6,15 +6,24 @@ import TodoEdit from './TodoEdit';
 function TodoInclusive() {
   const [todoShow, setTodoShow] = useState([]);
 
+  // useEffect(()=>{
+  //   localStorage.setItem('todoKey', JSON.stringify(todoShow))
+  // });
+
+  // useEffect(()=>{
+  //   const todoShow = JSON.parse(localStorage.getItem('todoKey'))
+  //   setTodoShow(todoShow)
+  // },[]);
+
   const addTodo = arr =>{
     setTodoShow([...todoShow,{id:crypto.randomUUID(),
     task:arr, completed : false, editTodo:false}]);
+    console.log(todoShow)
   }
 
   const todoComplete = id => {
     setTodoShow(todoShow.map(arr => arr.id === id ? 
         {...arr, completed:!arr.completed} : arr));
-        console.log('is working');
   }
   const deleteTodo = id => {
     setTodoShow(todoShow.filter(arr => arr.id !== id))
